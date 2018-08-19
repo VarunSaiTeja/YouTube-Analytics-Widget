7ea3ef6b5dc1168d6af015140ddabf3a075a004e
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2018-08-19 18:21:38 +0530

    Added Uninstaller

---
+++ Uninstaller/bye.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class bye
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(bye))
+        Me.title = New System.Windows.Forms.Label()
+        Me.button = New System.Windows.Forms.Button()
+        Me.tip = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'title
+        '
+        Me.title.AutoSize = True
+        Me.title.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.title.Location = New System.Drawing.Point(180, 9)
+        Me.title.Name = "title"
+        Me.title.Size = New System.Drawing.Size(145, 21)
+        Me.title.TabIndex = 0
+        Me.title.Text = "Confirm Uninstall"
+        '
+        'button
+        '
+        Me.button.Location = New System.Drawing.Point(210, 151)
+        Me.button.Name = "button"
+        Me.button.Size = New System.Drawing.Size(79, 28)
+        Me.button.TabIndex = 1
+        Me.button.Text = "Uninstall"
+        Me.button.UseVisualStyleBackColor = True
+        '
+        'tip
+        '
+        Me.tip.AutoSize = True
+        Me.tip.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.tip.Location = New System.Drawing.Point(51, 41)
+        Me.tip.Name = "tip"
+        Me.tip.Size = New System.Drawing.Size(369, 84)
+        Me.tip.TabIndex = 2
+        Me.tip.Text = resources.GetString("tip.Text")
+        '
+        'bye
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(487, 191)
+        Me.Controls.Add(Me.tip)
+        Me.Controls.Add(Me.button)
+        Me.Controls.Add(Me.title)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "bye"
+        Me.Text = "YouTube Analytics Widget Uninstaller"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents title As Label
+    Friend WithEvents button As Button
+    Friend WithEvents tip As Label
+End Class
---
+++ Uninstaller/bye.vb
@@ -0,0 +1,73 @@
+﻿Imports System.IO
+Imports Microsoft.Win32
+Imports System.Security.Principal
+
+Public Class bye
+    Dim reg_path As String = "HKEY_CURRENT_USER\Software\Varun\YouTube Analytics Widget"
+    Dim install_path As String = My.Computer.FileSystem.SpecialDirectories.ProgramFiles + "\Varun\YouTube Analytics Widget"
+
+    Public Function isAdmin()
+        Try
+            Dim user As WindowsIdentity = WindowsIdentity.GetCurrent
+            Dim principal As WindowsPrincipal = New WindowsPrincipal(user)
+            Return principal.IsInRole(WindowsBuiltInRole.Administrator)
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+    Private Sub button_Click(sender As Object, e As EventArgs) Handles button.Click
+        If button.Text = "Finish" Then
+            Process.Start("https://tiny.cc/whyytun")
+            Process.GetCurrentProcess.Kill()
+        End If
+
+        If button.Text = "Uninstall" Then
+            title.Text = "Uninstalling"
+            Try
+                Process.GetProcessesByName("YouTube Analytics Widget")(0).Kill()
+            Catch ex As Exception
+
+            End Try
+
+            Try
+                My.Computer.FileSystem.DeleteFile(install_path + "\YouTube Analytics Widget.exe")
+            Catch ex As Exception
+
+            End Try
+
+            Try
+                Dim regkey As Microsoft.Win32.RegistryKey
+                regkey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+                regkey.DeleteValue("YouTube Analytics Widget", False)
+                regkey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\Varun", True)
+                regkey.DeleteSubKey("YouTube Analytics Widget")
+                regkey.Close()
+            Catch ex As Exception
+
+            End Try
+
+            title.Text = "Uninstallation Done"
+            tip.Text = "By clicking on finish" + Environment.NewLine + "My Google form will open" + Environment.NewLine + "Please give feedback via that form"
+            button.Text = "Finish"
+        End If
+    End Sub
+
+    Private Sub bye_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If (isAdmin()) Then
+
+        Else
+            Try
+                Dim p As New Process
+                p.StartInfo.FileName = Application.ExecutablePath
+                p.StartInfo.Verb = "runas"
+                p.Start()
+                Process.GetCurrentProcess.Kill()
+            Catch ex As Exception
+                MsgBox("Need Admin Access")
+                Process.GetCurrentProcess.Kill()
+            End Try
+
+        End If
+    End Sub
+End Class
